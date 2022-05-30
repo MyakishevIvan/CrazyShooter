2292d51d1c87e0ef5a8e92b0f8e586e28a6d6d91
Ivan Myakishev <myakshevivan@gmail.com>
2022-05-30 10:49:11 +0300

    Правки по генерации комнат

--- CrazyShooter/Assets/Scripts/Config/RoomsConfig.cs
+++ CrazyShooter/Assets/Scripts/Config/RoomsConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using CrazyShooter.Enums;
 using CrazyShooter.Rooms;
 using UnityEngine;
 
@@ -8,7 +10,29 @@ namespace CrazyShooter.Configs
     public class RoomsConfig : ScriptableObject
     {
         [SerializeField] private List<BaseRoom> rooms;
+        [SerializeField] private RoomData roomsData;
+        private Dictionary<RoomType, BaseRoom> _roomsDict;
 
-        public List<BaseRoom> Rooms => rooms;
+        public Dictionary<RoomType, BaseRoom> RoomsDict => _roomsDict?? InitRoomsDictionaryDict();
+        public RoomData RoomsData => roomsData;
+
+        private Dictionary<RoomType, BaseRoom>  InitRoomsDictionaryDict()
+        {
+            _roomsDict = new Dictionary<RoomType, BaseRoom>();
+            
+            foreach (var room in rooms)
+                _roomsDict.Add(room.RoomType, room);
+            
+            return _roomsDict;
+        }
     }
+
+    [Serializable]
+    public class RoomData
+    {
+        public RoomType roomType;
+        public RoomDirectionType roomDirectionType;
+        public List<RoomData> dependentRooms;
+    }
+    
 }
--- CrazyShooter/Assets/Scripts/Controllers/FightSceneController.cs
+++ CrazyShooter/Assets/Scripts/Controllers/FightSceneController.cs
@@ -1,12 +1,14 @@
+using System.Collections.Generic;
 using CrayzShooter.Configs;
 using CrayzShooter.Core;
 using CrayzShooter.Enum;
+using CrazyShooter.Configs;
 using CrazyShooter.Enums;
 using CrazyShooter.Rooms;
 using UnityEngine;
 using Zenject;
 
-namespace CrayzShooter.FightScene
+namespace CrazyShooter.FightScene
 {
     public class FightSceneController : MonoBehaviour
     {
@@ -14,44 +16,65 @@ namespace CrayzShooter.FightScene
         [Inject] private BalanceStorage _balanceStorage;
         private WeaponType _weaponType = WeaponType.Gun;
 
-        private PlayerConfig _playerConfig => _balanceStorage.PlayerConfig;
-
+        private PlayerConfig PlayerConfig => _balanceStorage.PlayerConfig;
+        private Dictionary<RoomType, BaseRoom> RoomsDict => _balanceStorage.RoomsConfig.RoomsDict;
         private void Awake()
         {
-            InitRooms();
+            InitSpawnRoom();
             InitPlayer();
         }
 
         private void InitPlayer()
         {
-            var player = _diContainer.InstantiatePrefabForComponent<PlayerView>(_playerConfig.Player);
+            var player = _diContainer.InstantiatePrefabForComponent<PlayerView>(PlayerConfig.Player);
             var playerController =
-                _diContainer.InstantiatePrefabForComponent<PlayerController>(_playerConfig.PlayerController, player.transform);
+                _diContainer.InstantiatePrefabForComponent<PlayerController>(PlayerConfig.PlayerController,
+                    player.transform);
             playerController.UpdageControllerView(_weaponType);
             player._playerController = playerController;
             var weapon = _balanceStorage.WeaponsConfig.GetWeapon(_weaponType);
             player.InitWeapon(weapon, playerController.ShootJoystick);
         }
 
-        private void InitRooms()
+        private void InitSpawnRoom(RoomData roomsData = null )
         {
-            var rooms = _balanceStorage.RoomsConfig.Rooms;
-
-            BaseRoom previouseRoom = null;
-            foreach (var room in rooms)
+            roomsData ??= _balanceStorage.RoomsConfig.RoomsData;
+            BaseRoom currentRoom = null;
+            
+            if (roomsData.roomType == RoomType.SpawnRoom)
             {
-                if (room.RoomType == RoomType.SpawnRoom)
-                {
-                    previouseRoom = _diContainer.InstantiatePrefabForComponent<SpawnRoom>(room, transform);
-                }
-                else
-                {
-                    var instantiatedRoom = _diContainer.InstantiatePrefabForComponent<EnemyRoom>(room,transform);
-                    var pos = previouseRoom.GetRoomPosition(instantiatedRoom, RoomDirectionType.Bottom);
-                    instantiatedRoom.transform.localPosition = pos;
-                }
-                    
+                currentRoom =
+                    _diContainer.InstantiatePrefabForComponent<SpawnRoom>(RoomsDict[roomsData.roomType], transform);
             }
+            else
+            {
+                Debug.LogError("First room is not Spawn Room!");
+            }
+
+            if (roomsData.dependentRooms.Count == 0)
+                return;
+            
+            InitDependentRooms(roomsData.dependentRooms, currentRoom);
+        }
+
+        private void InitDependentRooms(List<RoomData> dependentRooms, BaseRoom previouseRoom)
+        {
+            foreach (var room in dependentRooms)
+            {
+                var instantiatedRoom =
+                    _diContainer.InstantiatePrefabForComponent<EnemyRoom>(RoomsDict[room.roomType], transform);
+                var pos = previouseRoom.GetRoomPosition(instantiatedRoom, room.roomDirectionType);
+                instantiatedRoom.transform.localPosition = pos;
+                
+                if (room.dependentRooms.Count == 0)
+                    continue;
+            
+                InitDependentRooms(room.dependentRooms, instantiatedRoom);
+            }
+
+
+            // if (roomData.dependentRooms.Count > 0)
+            //     InitDependentRooms(roomData.dependentRooms);
         }
     }
 }
